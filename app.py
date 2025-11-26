737c604cfb18b509156fef9b266943f2d535e03c
suhyeok <133014897+suhyeoked@users.noreply.github.com>
2025-11-26 11:10:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, CI/CD World! v1.0</h1>'
+    return '<h1>Hello, CI/CD World! v2.0 - Updated!!</h1>' # <- 수정된 부분
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
